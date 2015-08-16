c86cf010804a0f4a7c8947f26055393b05875e03
SvetZari <mj12.svetzari@gmail.com>
2015-08-16 21:27:46 +0300

    Code refactoring

--- Chess/main.cpp
+++ Chess/main.cpp
@@ -1,4 +1,5 @@
 #include "logiccontroller.h"
+#include "chessmove.h"
 #include "model/abstractfigure.h"
 
 #include <QApplication>
@@ -6,14 +7,23 @@
 #include <QQmlEngine>
 #include <QtQml>
 
+void initQmlTypes()
+{
 
+    qmlRegisterType<LogicController>("Chess.Logic", 1, 0, "Logic");
+    qmlRegisterType<AbstractFigure>("Chess.Figure", 1, 0, "Figure");
+    qmlRegisterType<ChessMove>("Chess.Move", 1, 0, "Move");
+}
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+    initQmlTypes();
     QQmlApplicationEngine engine;
     QQmlContext* ctx = engine.rootContext();
+
+    LogicController controller;
     ctx->setContextProperty("controller", &controller);
     engine.load(QUrl(QStringLiteral("qrc:/chess.qml")));
 
